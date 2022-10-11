91b3ba15fbdb55d28e88435a53fbdf49e6a65026
CoderSah <115417419+CoderSah@users.noreply.github.com>
2022-10-11 23:16:31 +0530

    Added data and script files

---
+++ Landing-Page-Files/data.js
@@ -0,0 +1,39 @@
+// Instruction to add projects
+// in name add the directory Name of the project onplay
+// in author add the github username
+// if the project is static then in is_static write true
+// If the project is dynamic then in is_static write false so it will not render preview button
+// Important : main html file in the project should be named index.html only 
+
+const projects=[
+    {
+        name:"Animated-login",
+        author:"unknown2k21",
+        is_static:true
+    },
+    {
+        name:"Form with Validation",
+        author:"Nitishpal68",
+        is_static:true
+    },
+    {
+        name:"Login-page validation",
+        author:"unknown2k21",
+        is_static:true
+    },
+    {
+        name:"Simple Web Page",
+        author:"unknown2k21",
+        is_static:false
+    },
+    {
+        name:"Weather-app",
+        author:"unknown2k21",
+        is_static:true
+    },
+    {
+        name:"Weather",
+        author:"unknown2k21",
+        is_static:true
+    }
+]
